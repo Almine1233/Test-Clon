b75176523546f0ea46beebd8d92b2e9407807102
Almine1233 <alanbedoyagomes@gmail.com>
2025-12-11 21:16:50 +0100

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,8 @@
+const productos = [
+  {id:1, nombre:"Laptop Gamer", precio:1200, imagen:"decoraciones/laptop.jpg", categoria:"Electrónica"},
+  {id:2, nombre:"Smartphone", precio:800, imagen:"decoraciones/phone.jpg", categoria:"Electrónica"},
+  {id:3, nombre:"Auriculares", precio:150, imagen:"decoraciones/headphones.jpg", categoria:"Electrónica"},
+  {id:4, nombre:"Reloj Inteligente", precio:200, imagen:"decoraciones/watch.jpg", categoria:"Electrónica"},
+  {id:5, nombre:"Zapatillas Deportivas", precio:120, imagen:"decoraciones/shoes.jpg", categoria:"Deportes"},
+  {id:6, nombre:"Cafetera", precio:85, imagen:"decoraciones/coffeemachine.jpg", categoria:"Hogar"},
+];
